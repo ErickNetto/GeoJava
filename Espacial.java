090eaed1a92a85721cd4cf1f8bbe6890ce14e4e6
adrielyrossetti <108466648+adrielyrossetti@users.noreply.github.com>
2024-05-28 12:43:37 -0300

    Update Espacial.java

--- Espacial.java
+++ Espacial.java
@@ -7,23 +7,23 @@ public class Espacial extends Plano{
         System.out.println("Deseja calcular o volume ou a área total?");
         System.out.println("1.Volume");
         System.out.println("2.Área total");
-        int volouat = input.nextInt();
+        int volouat = ValidaInt();
         Double areaBasePrisma;
         switch (volouat) {
             case 1:
             System.out.println("Qual é a área da base?");
-            areaBasePrisma = input.nextDouble();
+            areaBasePrisma = ValidaDouble();
             System.out.println("Qual é a altura do prisma?");
-            Double alturaPrisma = input.nextDouble();
+            Double alturaPrisma = ValidaDouble();
             Double volumePrisma = areaBasePrisma*alturaPrisma;
             System.out.println("O volume do prisma é de "+volumePrisma+" m³");
                 break;
             
             case 2:
             System.out.println("Qual é a área da base?");
-            areaBasePrisma = input.nextDouble();
+            areaBasePrisma = ValidaDouble();
             System.out.println("Qual é a área lateral?");
-            Double areaLateralPrisma = input.nextDouble();
+            Double areaLateralPrisma = ValidaDouble();
             Double areaTotalPrisma = areaBasePrisma+areaLateralPrisma;
             System.out.println("A área total do prisma é de "+areaTotalPrisma+" m²");
                 break;
